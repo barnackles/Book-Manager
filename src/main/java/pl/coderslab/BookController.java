e0ece260d1574e287e60c8c49a88aebee7decead
barnackles <p.martewicz@yahoo.com>
2022-06-18 14:59:04 +0200

    Setup

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,15 +1,24 @@
 package pl.coderslab;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    @RequestMapping("/helloBook")
-    public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java",
-                "Bruce Eckel", "Helion", "programming");
+    @GetMapping("/books")
+    public List<Book> showAllBooks(MockBookService mockBookService) {
+        return mockBookService.getList();
     }
+
+
+
+
+//    @RequestMapping("/helloBook")
+//    public Book helloBook() {
+//        return new Book(1L, "9788324631766", "Thinking in Java",
+//                "Bruce Eckel", "Helion", "programming");
+//    }
 }
